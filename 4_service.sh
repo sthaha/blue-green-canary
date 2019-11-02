67736b091a00518e77363fd3d5302b71bcfa7365
Burr Sutter <burrsutter@gmail.com>
2019-11-02 11:01:12 +0200

    Здравствуйте

--- 4_service.sh
+++ 4_service.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 
 kubectl create -f src/main/kubernetes/Service.yml
-kubectl create -f src/main/istio/Virtual_service_blue.yml
--- 6_deploy_gateway.sh
+++ 6_deploy_gateway.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-kubectl apply -f src/main/istio/Gateway_VS.yml
+kubectl create -f src/main/istio/Gateway_VS_Openshift.yml
 kubectl create -f src/main/istio/Destination_rule_blue_green.yml
