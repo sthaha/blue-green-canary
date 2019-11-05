31e7a19754be4816983ca9f19d3a0f09c6e08bb1
Burr Sutter <burrsutter@gmail.com>
2019-11-05 11:26:23 -0500

    jambo

--- src/main/java/com/burrsutter/bluegreencanary/APIResource.java
+++ src/main/java/com/burrsutter/bluegreencanary/APIResource.java
@@ -28,7 +28,7 @@ public class APIResource {
   // String color = "#5bbf45"; // green
   String color = "#f2f25e"; // canary
 
-  String hello = "Здравствуйте";
+  String hello = "Jambo";
 
   String hostname = System.getenv().getOrDefault("HOSTNAME", "unknown");
   // Map<String, HttpSession> sessions = new ConcurrentHashMap<>();
