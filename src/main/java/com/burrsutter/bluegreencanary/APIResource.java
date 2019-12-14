a9b3d16ccfba9ef9b63b5be3737254dd49c8e5a9
Burr Sutter <burrsutter@gmail.com>
2019-12-14 20:34:44 +0200

    howdy & scripts

--- src/main/java/com/burrsutter/bluegreencanary/APIResource.java
+++ src/main/java/com/burrsutter/bluegreencanary/APIResource.java
@@ -28,7 +28,7 @@ public class APIResource {
   // String color = "#5bbf45"; // green
   String color = "#f2f25e"; // canary
 
-  String hello = "Aloha";
+  String hello = "Howdy";
 
   String hostname = System.getenv().getOrDefault("HOSTNAME", "unknown");
   // Map<String, HttpSession> sessions = new ConcurrentHashMap<>();
