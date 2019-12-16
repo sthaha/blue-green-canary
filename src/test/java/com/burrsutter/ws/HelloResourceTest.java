acc2405cc39aa4c0ed824f6eb83fe85b893ca12f
Burr Sutter <burrsutter@gmail.com>
2019-12-16 13:00:03 +0200

    hola

--- src/test/java/com/burrsutter/ws/HelloResourceTest.java
+++ src/test/java/com/burrsutter/ws/HelloResourceTest.java
@@ -16,7 +16,7 @@ public class HelloResourceTest {
           .when().get("/api")
           .then()
              .statusCode(200)
-             .body(containsString("Shalom"));
+             .body(containsString("Hola"));
     }
 
 }
